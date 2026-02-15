1703ba47d91880ad1323b8060899317facf769ef
Joshua Carroll <carrolljosh1976@gmail.com>
2026-02-15 16:43:39 -0500

    add hero section and hero visual component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { HeroSection } from "@/components/ui/landing/hero-section";
 import { LandingHeader } from "@/components/ui/landing/landing-header";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white dark:bg-zinc-950">
       <LandingHeader />
-      {/*<HeroSection />
-      <FeaturesSection />
+      <HeroSection />
+      {/*<FeaturesSection />
       <HowItWorksSection />
       <IntegrationSection />
       <CTASection />*/}
