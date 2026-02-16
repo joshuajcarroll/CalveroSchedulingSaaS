6ca723848ce99772283f6f68c20472b5a0a0f3cc
Joshua Carroll <carrolljosh1976@gmail.com>
2026-02-16 15:02:20 -0500

    added sections for landing page and features card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import { CTASection } from "@/components/ui/landing/cta-section";
+import { FeaturesSection } from "@/components/ui/landing/features-section";
 import { HeroSection } from "@/components/ui/landing/hero-section";
+import { HowItWorksSection } from "@/components/ui/landing/how-it-works-section";
+import { IntegrationSection } from "@/components/ui/landing/integration-section";
 import { LandingHeader } from "@/components/ui/landing/landing-header";
 
 export default function Home() {
@@ -6,10 +10,10 @@ export default function Home() {
     <div className="min-h-screen bg-white dark:bg-zinc-950">
       <LandingHeader />
       <HeroSection />
-      {/*<FeaturesSection />
+      <FeaturesSection />
       <HowItWorksSection />
       <IntegrationSection />
-      <CTASection />*/}
+      <CTASection />
     </div>
   );
 }
